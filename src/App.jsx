e674eb52ba6f91fc3dcf990dc9109a516a46a758
ashwin-1-git <ashwinttthampan@gmail.com>
2025-11-17 22:45:03 +0530

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route ,Routes} from 'react-router-dom'
 import './App.css'
 import Home from "./pages/Home"
-import Home from "./pages/Wishlist"
-import Home from "./pages/Cart"
-import Home from "./pages/View"
-import Home from "./pages/Pnf"
+import Cart from "./pages/Cart"
+import View from "./pages/View"
 import Wishlist from './pages/Wishlist'
 import Pnf from './pages/Pnf'
+// import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 
@@ -23,6 +23,7 @@ function App() {
       <Route path='/*' element={<Pnf/>}/>
 
     </Routes>
+    <Footer/>
     </>
   )
 }
